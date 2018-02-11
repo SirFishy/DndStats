946d7fcd5e36f95eb55bdae7f516879d423b55de
SirFishy <khfishy@gmail.com>
2018-02-11 10:49:04 -0500

    Added templates directory, index.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+import flask
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Kristian'}
+    return flask.render_template('index.html', title='Home', user=user)
