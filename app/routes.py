fe7732b136203ad437db5b66f80081a01c9f6c10
SirFishy <khfishy@gmail.com>
2018-02-11 13:36:39 -0500

    Added simple display with hardcoded stats

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from app import app
 import flask
+import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
+    today = datetime.date.today()
     user = {'username': 'Kristian'}
-    return flask.render_template('index.html', title='Home', user=user)
+    stats = [
+        {
+            'date': {'date': today, 'displaydate': today.strftime("%A %d. %B %Y")},
+            'session': '1'
+        },
+        {
+            'date': {'date': today, 'displaydate': today.strftime("%A %d. %B %Y")},
+            'session': '1'
+        }
+    ]
+    return flask.render_template('index.html', title='Home', user=user, stats=stats)
