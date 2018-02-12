9354d2c394536c853fbd30e6b40ae1220794992b
SirFishy <khfishy@gmail.com>
2018-02-12 11:45:08 -0500

    Changes to project to move to pycharm. Added base.html to serve as the base for web pages.

--- app/dndstats.py
+++ app/dndstats.py
@@ -1,1 +1,25 @@
-from app import app
+import flask
+import datetime
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    today = datetime.date.today()
+    user = {'username': 'Kristian'}
+    stats = [
+        {
+            'date': {'date': today, 'displaydate': today.strftime("%A %d. %B %Y")},
+            'session': '1'
+        },
+        {
+            'date': {'date': today, 'displaydate': today.strftime("%A %d. %B %Y")},
+            'session': '1'
+        }
+    ]
+    return flask.render_template('index.html', title='Home', user=user, stats=stats)
+
+
+if __name__ == '__main__':
+    app.run()
