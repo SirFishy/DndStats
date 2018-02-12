20ba77b45081299557a673fbcecb277d863bb689
SirFishy <khfishy@gmail.com>
2018-02-12 14:12:33 -0500

    Add validation and error display.

--- app/dndstats.py
+++ app/dndstats.py
@@ -3,9 +3,9 @@ import datetime
 from app.forms import LoginForm
 from config import Config
 
-
 app = flask.Flask(__name__)
 app.config.from_object(Config)
+
 
 @app.route('/')
 @app.route('/index')
@@ -24,10 +24,17 @@ def index():
     ]
     return flask.render_template('index.html', title='Home', user=user, stats=stats)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flask.flash('Login requested for user {}, remember me ={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return flask.redirect(flask.url_for('index'))
     return flask.render_template('login.html', title='Sign In', form=form)
+
 
 if __name__ == '__main__':
     app.run()
