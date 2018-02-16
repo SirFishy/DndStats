ebb9b805550c85577cc34851dbb215adc2553c94
SirFishy <khfishy@gmail.com>
2018-02-16 09:16:33 -0500

    Changes to make sure flask runs correctly in pycharm and on CLI

--- dndstats.py
+++
@@ -1,6 +0,0 @@
-import app
-
-if __name__ == "__main__":
-    flask_app = app.create_app()
-    flask_app.run()
-
---
+++ server.py
@@ -0,0 +1,7 @@
+import app
+
+app = app.create_app()
+
+if __name__ == "__main__":
+    app.run()
+
